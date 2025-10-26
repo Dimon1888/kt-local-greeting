6d9914f9012661dda16042252f019afd6d2b7f85
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-10-26 22:25:03 +0100

    Task completed

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,6 +1,26 @@
 package mate.academy
-
+//У цьому завданні створіть локальну функцію, яка:
+//запитує у користувача ім'я (з консолі),]
+// доки не буде введено порожній рядок;
+//друкує привітання для кожного імені , наприклад, Hello, Alex!.
+//Ось приклад послідовності виконання програми:
+//Please enter a name (or press Enter to quit):
+//Bob
+//Hello, Bob!
+//Please enter a name (or press Enter to quit):
+//Alice
+//Hello, Alice!
+//Process finished with exit code 0
 fun greetUsers() {
+    var name: String? = null
+    do {
+        print("Please enter a name (or press Enter toguit): ")
+        name = readLine()
+        val trimmedName = name?.trim()
+        if (!trimmedName.isNullOrEmpty()) {
+            println("Hello, $trimmedName!")
+        }
+    } while (!trimmedName.isNullOrEmpty())
     // implement function
 }
 
